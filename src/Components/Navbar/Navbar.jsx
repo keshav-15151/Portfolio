5a23d3943cc3010d265fb430bab8d611d08f8fa9
Keshav Sharma <keshav15151@gmail.com>
2024-11-10 11:44:21 +0530

    Update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
+
 const Navbar = () => {
   const [menu, setMenu] = useState("home")
   return (
     <div className='navbar'>
+
         <ul className='nav-menu'>
             <li><AnchorLink className='anchor-link' href='#home'><p onClick={() => setMenu("home")}>Home</p></AnchorLink></li>
             <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() => setMenu("about")}>About Me</p></AnchorLink></li>
